fb8cc5569ae79a91ad1ac336ad4bfda3f24f4f08
ripee <ripee.dev@gmail.com>
2020-06-09 13:44:08 -0700

    Revert "hidl: powershare: add vintf manifest fragment"
    This reverts commit cbdc389458c9a6a5291a23d1252a202fadd412f0.

--- hidl/powershare/Android.mk
+++ hidl/powershare/Android.mk
@@ -38,7 +38,6 @@ LOCAL_INIT_RC := vendor.lineage.powershare@1.0-service.samsung.rc
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_OWNER := samsung
-LOCAL_VINTF_FRAGMENTS := vendor.lineage.powershare@1.0-service.samsung.xml
 LOCAL_VENDOR_MODULE := true
 
 include $(BUILD_EXECUTABLE)
